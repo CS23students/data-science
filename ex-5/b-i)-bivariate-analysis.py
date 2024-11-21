0ff7cda0ff5c57d0ab48dfada2c867fd5c1fcad0
CS23students <153345000+CS23students@users.noreply.github.com>
2024-11-22 00:13:11 +0530

    Update b-i)-bivariate-analysis.py

--- ex-5/b-i)-bivariate-analysis.py
+++ ex-5/b-i)-bivariate-analysis.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 d = pd.read_csv(
     r"C:\Users\cse\Documents\Data science\ex-5\diabetes.csv")
-x = d.iloc[:5].values.reshape(-1, 1)
-y = x = d.iloc[:1].values.reshape(-1, 1)
+x = d.iloc[:5,-1].values.reshape(-1, 1)
+y = d.iloc[:1,1].values.reshape(-1, 1)
 
 lin = LinearRegression()
 lin.fit(x, y)
