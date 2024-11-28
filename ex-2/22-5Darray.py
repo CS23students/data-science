6f16e0a2fc6d00b6898c20f17d09bab0dbd1ec51
CS23students <153345000+CS23students@users.noreply.github.com>
2024-11-28 21:32:43 +0530

    Update 22-5Darray.py

--- ex-2/22-5Darray.py
+++ ex-2/22-5Darray.py
@@ -1,7 +1,14 @@
 import numpy as np
 
-array = np.random.rand(2, 3, 4, 5, 6)
-num_dim = array.ndim
-print("Array shape: ", array.shape)
-print("Number of dimensions: ", num_dim)
+# Creating an array and reshaping it to 5 dimensions (1 element per dimension in this example)
+array = np.array([2, 3, 4, 5, 6]).reshape(1, 1, 1, 1, 5)
+
+print("Array shape: ", array.shape)                 # Outputs the shape of the array
+print("Number of dimensions: ", array.ndim)         # Prints the number of dimensions
 print(array)
+
+
+# OUTPUT:
+# Array shape:  (1, 1, 1, 1, 5)
+# Number of dimensions:  5
+# [[[[[2 3 4 5 6]]]]]
